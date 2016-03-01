e75447360197302fd1b8980b70278b049e6d9c71
Lander Brandt <landerbrandt@gmail.com>
2016-02-29 23:58:44 -0800

    Add recursive watcher, protections against empty event names

--- cmd/fsync/internal/watcher.go
+++ cmd/fsync/internal/watcher.go
@@ -1,6 +1,12 @@
 package internal
 
-import "gopkg.in/fsnotify.v1"
+import (
+	"os"
+	"strings"
+
+	"github.com/landaire/recwatch"
+	"gopkg.in/fsnotify.v1"
+)
 
 var (
 	WatchExit chan bool
@@ -13,18 +19,21 @@ func init() {
 // Watch watches the given path for changes using fsnotify and will upload
 // files as needed
 func Watch(path string) {
-	watcher, err := fsnotify.NewWatcher()
+	Log.Debugf("Adding %s to watcher\n", path)
+	watcher, err := recwatch.NewRecursiveWatcher(path)
 	if err != nil {
 		Log.Fatalf("Could not initialize fsnotify watcher: %v\n", err)
 	}
-
-	Log.Debugf("Adding %s to watcher\n", path)
-	watcher.Add(path)
 
 	for {
 		select {
 		case event := <-watcher.Events:
 			Log.Debugln("Event:", event)
+			if strings.TrimSpace(event.Name) == "" {
+				Log.Debugln("Got an empty string... not touching that")
+				continue
+			}
+
 			if event.Op&fsnotify.Write == fsnotify.Write {
 				Log.Debugln("Write event received:", event.Name)
 				ModifiedFiles <- event.Name
@@ -34,6 +43,18 @@ func Watch(path string) {
 			} else if event.Op&fsnotify.Create == fsnotify.Create {
 				Log.Debugln("Create event received:", event.Name)
 				ModifiedFiles <- event.Name
+
+				stat, err := os.Stat(event.Name)
+				if err != nil {
+					Log.Errorln(err)
+					continue
+				}
+
+				if stat.IsDir() {
+					if err := watcher.Add(event.Name); err != nil {
+						Log.Debugln("Couldn't watch folder:", err)
+					}
+				}
 			} else if event.Op&fsnotify.Rename == fsnotify.Rename {
 				Log.Debugln("Rename event received:", event.Name)
 				DeletedFiles <- event.Name
